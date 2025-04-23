5a6259e830568ca592e949972f35528b2c376221
iamhaiderabbas2003 <114867851+hAI-FA001@users.noreply.github.com>
2025-04-23 16:50:27 +0000

    add conversation state (prevent chatting if message already in process), progressively handle each chunk of response (log, show in UI, handle any function calls)

--- lib/services/gemini_chat_service.dart
+++ lib/services/gemini_chat_service.dart
@@ -10,46 +10,41 @@ import 'gemini_tools.dart';
 
 part 'gemini_chat_service.g.dart';
 
+final conversationStateProvider = StateProvider(
+  (ref) => ConversationState.idle,
+);
+
 class GeminiChatService {
   GeminiChatService(this.ref);
   final Ref ref;
 
   Future<void> sendMessage(String message) async {
     final chatSession = await ref.read(chatSessionProvider.future);
+    final conversationState = ref.read(conversationStateProvider);
     final chatStateNotifier = ref.read(chatStateNotifierProvider.notifier);
     final logStateNotifier = ref.read(logStateNotifierProvider.notifier);
 
+    if (conversationState == ConversationState.busy) {
+      logStateNotifier.logWarning(
+        "Can't send a message while a conversation is in progress",
+      );
+      throw Exception(
+        "Can't send a message while a conversation is in progress",
+      );
+    }
+    final conversationStateNotifier = ref.read(
+      conversationStateProvider.notifier,
+    );
+    conversationStateNotifier.state = ConversationState.busy;
     chatStateNotifier.addUserMessage(message);
     logStateNotifier.logUserText(message);
     final llmMessage = chatStateNotifier.createLlmMessage();
     try {
-      final response = await chatSession.sendMessage(Content.text(message));
-
-      final responseText = response.text;
-      if (responseText != null) {
-        logStateNotifier.logLlmText(responseText);
-        chatStateNotifier.appendToMessage(llmMessage.id, responseText);
-      }
-
-      if (response.functionCalls.isNotEmpty) {
-        final geminiTools = ref.read(geminiToolsProvider);
-        final functionResultResponse = await chatSession.sendMessage(
-          Content.functionResponses([
-            for (final functionCall in response.functionCalls)
-              FunctionResponse(
-                functionCall.name,
-                geminiTools.handleFunctionCall(
-                  functionCall.name,
-                  functionCall.args,
-                ),
-              ),
-          ]),
-        );
-        final responseText = functionResultResponse.text;
-        if (responseText != null) {
-          logStateNotifier.logLlmText(responseText);
-          chatStateNotifier.appendToMessage(llmMessage.id, responseText);
-        }
+      final responseStream = chatSession.sendMessageStream(
+        Content.text(message),
+      );
+      await for (final block in responseStream) {
+        await _processBlock(block, llmMessage.id);
       }
     } catch (e, st) {
       logStateNotifier.logError(e, st: st);
@@ -60,6 +55,45 @@ class GeminiChatService {
       );
     } finally {
       chatStateNotifier.finalizeMessage(llmMessage.id);
+      conversationStateNotifier.state = ConversationState.idle;
+    }
+  }
+
+  Future<void> _processBlock(
+    GenerateContentResponse block,
+    String llmMessageId,
+  ) async {
+    final chatSession = await ref.read(chatSessionProvider.future);
+    final chatStateNotifier = ref.read(chatStateNotifierProvider.notifier);
+    final logStateNotifier = ref.read(logStateNotifierProvider.notifier);
+    final blockText = block.text;
+
+    if (blockText != null) {
+      logStateNotifier.logLlmText(blockText);
+      chatStateNotifier.appendToMessage(llmMessageId, blockText);
+    }
+
+    if (block.functionCalls.isNotEmpty) {
+      final geminiTools = ref.read(geminiToolsProvider);
+      final responseStream = chatSession.sendMessageStream(
+        Content.functionResponses([
+          for (final functionCall in block.functionCalls)
+            FunctionResponse(
+              functionCall.name,
+              geminiTools.handleFunctionCall(
+                functionCall.name,
+                functionCall.args,
+              ),
+            ),
+        ]),
+      );
+      await for (final response in responseStream) {
+        final responseText = response.text;
+        if (responseText != null) {
+          logStateNotifier.logLlmText(responseText);
+          chatStateNotifier.appendToMessage(llmMessageId, responseText);
+        }
+      }
     }
   }
 }
