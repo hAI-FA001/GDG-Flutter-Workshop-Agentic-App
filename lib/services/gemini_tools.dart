26ee99b8a537ffa54a46c41a28c886e645187e68
iamhaiderabbas2003 <114867851+hAI-FA001@users.noreply.github.com>
2025-04-23 16:40:25 +0000

    implement tool output handling, handle unknown tools also

--- lib/services/gemini_tools.dart
+++ lib/services/gemini_tools.dart
@@ -1,3 +1,4 @@
+import 'package:colorist_ui/colorist_ui.dart';
 import 'package:firebase_vertexai/firebase_vertexai.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -22,6 +23,45 @@ class GeminiTools {
   List<Tool> get tools => [
     Tool.functionDeclarations([setColorFuncDecl]),
   ];
+
+  Map<String, Object?> handleFunctionCall(
+    String functionName,
+    Map<String, Object?> arguments,
+  ) {
+    final logStateNotifier = ref.read(logStateNotifierProvider.notifier);
+    logStateNotifier.logFunctionCall(functionName, arguments);
+    return switch (functionName) {
+      'set_color' => handleSetColor(arguments),
+      _ => handleUnknownFunction(functionName),
+    };
+  }
+
+  Map<String, Object?> handleSetColor(Map<String, Object?> arguments) {
+    final colorStateNotifier = ref.read(colorStateNotifierProvider.notifier);
+    final red = (arguments['red'] as num).toDouble();
+    final green = (arguments['green'] as num).toDouble();
+    final blue = (arguments['blue'] as num).toDouble();
+    final functionResults = {
+      'success': true,
+      'current_color':
+          colorStateNotifier
+              .updateColor(red: red, green: green, blue: blue)
+              .toLLMContextMap(),
+    };
+
+    final logStateNotifier = ref.read(logStateNotifierProvider.notifier);
+    logStateNotifier.logFunctionResults(functionResults);
+    return functionResults;
+  }
+
+  Map<String, Object?> handleUnknownFunction(String functionName) {
+    final logStateNotifier = ref.read(logStateNotifierProvider.notifier);
+    logStateNotifier.logWarning('Unsupported function call $functionName');
+    return {
+      'success': false,
+      'reason': 'Unsupported function call $functionName',
+    };
+  }
 }
 
 @riverpod
